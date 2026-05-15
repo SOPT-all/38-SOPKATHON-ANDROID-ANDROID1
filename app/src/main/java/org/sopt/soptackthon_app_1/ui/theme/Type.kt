ff411fe2066372c0dfcfc30030337baae2249e55
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-15 13:12:44 +0900

    [feat/#1] DS 세팅

--- app/src/main/java/org/sopt/soptackthon_app_1/ui/theme/Type.kt
+++ app/src/main/java/org/sopt/soptackthon_app_1/ui/theme/Type.kt
@@ -1,34 +1,34 @@
-package org.sopt.soptackthon_app_1.ui.theme
-
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.sopt.soptackthon_app_1.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+val SopkathonRegularFont = FontFamily(Font(R.font.pretendard_regular))
+
+@Immutable
+data class SopkathonTypography(
+    val title: TextStyle,
+    // 여기에 추가 해주시면 돼용
 )
+
+val defaultSopkathonTypography = SopkathonTypography(
+    title = TextStyle(
+        fontFamily = SopkathonRegularFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    // 여기에 추가
+//    title_24Strong = TextStyle(
+//    fontFamily = HsLinkSemiBoldFont,
+//    fontWeight = FontWeight.SemiBold,
+//    fontSize = 24.sp,
+//    lineHeight = 34.sp,
+//    letterSpacing = (-0.03).em
+)
+
+val LocalSopkathonTypography = staticCompositionLocalOf { defaultSopkathonTypography }
