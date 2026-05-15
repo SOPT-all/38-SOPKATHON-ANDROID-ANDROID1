53c272e6c6f1104516e41458a5c0aab94580c8f1
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-15 20:57:11 +0900

    [init/#1] Service 세팅

---
+++ app/src/main/java/org/sopt/soptackthon_app_1/data/service/DummyService.kt
@@ -0,0 +1,18 @@
+package org.sopt.soptackthon_app_1.data.service
+
+import org.sopt.soptackthon_app_1.data.dto.base.BaseResponse
+import org.sopt.soptackthon_app_1.data.dto.requset.DummyRequestDto
+import org.sopt.soptackthon_app_1.data.dto.response.DummyResponseDto
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface DummyService {
+    @GET("api/v1/data")
+    suspend fun getExampleData(): BaseResponse<DummyResponseDto>
+
+    @POST("api/v1/data")
+    suspend fun postExampleData(
+        @Body exampleRequestDto: DummyRequestDto,
+    ): BaseResponse<Unit>
+}
