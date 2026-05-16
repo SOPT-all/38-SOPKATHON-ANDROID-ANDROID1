35d03a04a98e86938f82a6ac49cc97884d0b950a
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-16 11:05:06 +0900

    [feat] UIstate 예시 추가

--- app/src/main/java/org/sopt/soptackthon_app_1/presentation/yerim/YerimUiState.kt
+++ app/src/main/java/org/sopt/soptackthon_app_1/presentation/yerim/YerimUiState.kt
@@ -1,7 +1,11 @@
 package org.sopt.soptackthon_app_1.presentation.yerim
 
 data class YerimUiState(
-    val title: String = ""
+    val title: String = "",
+    //변수들 추가해주세용
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val isSuccess: Boolean = false,
 ) {
 
 }
--- app/src/main/java/org/sopt/soptackthon_app_1/presentation/yubin2/Yubin2UiState.kt
+++ app/src/main/java/org/sopt/soptackthon_app_1/presentation/yubin2/Yubin2UiState.kt
@@ -1,7 +1,11 @@
 package org.sopt.soptackthon_app_1.presentation.yubin2
 
 data class Yubin2UiState(
-    val title: String = ""
+    val title: String = "",
+    //변수들 추가해주세용
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val isSuccess: Boolean = false,
 ) {
 
 }
