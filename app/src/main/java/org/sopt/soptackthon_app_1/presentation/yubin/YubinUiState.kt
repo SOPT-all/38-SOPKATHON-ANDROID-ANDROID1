f279c45845a1464b994b769bc11468032c249fbd
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-17 03:55:32 +0900

    [feat/#16] 유빈스크림 기록 화면 구현

--- app/src/main/java/org/sopt/soptackthon_app_1/presentation/yubin/YubinUiState.kt
+++ app/src/main/java/org/sopt/soptackthon_app_1/presentation/yubin/YubinUiState.kt
@@ -1,11 +1,11 @@
 package org.sopt.soptackthon_app_1.presentation.yubin
 
+import android.net.Uri
+
 data class YubinUiState(
     val title: String = "",
-    //변수들 추가해주세용
+    val imageUri: Uri? = null,
     val isLoading: Boolean = false,
-    val error: String? = null,
     val isSuccess: Boolean = false,
-) {
-
-}
+    val error: String? = null,
+)
